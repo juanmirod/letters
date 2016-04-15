8ebe0ca0df154f5665576e705105bd3b29a30e0d
Juanmi Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2016-04-16 00:45:09 +0200

    Building service to get the words and the shuffles

--- src/words.service.js
+++ src/words.service.js
@@ -1,12 +1,50 @@
 var WordsService = (function(){
 
+  var views = Array.apply(null, Array(Words.length))
+    .map(Number.prototype.valueOf, 0);
+  var lastWord = -1;
+
+  var min = function(array) { 
+    return array.indexOf(Math.min.apply(null, array));
+  }
+
+  /* Takes the letters from the word provided and returns 
+    a new string with the letters shuffled 
+  */
+  var shuffle = function(word) {
+    
+    var wordLetters = word.split(""),
+        n = wordLetters.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [wordLetters[i], wordLetters[j]] = 
+        [wordLetters[j], wordLetters[i]];
+    }
+
+    return wordLetters.join("");
+
+  }
+
   return {
+
+    init: function() {
+      views = Array.apply(null, Array(Words.length))
+        .map(Number.prototype.valueOf, 0);
+      lastWord = -1;
+    },
 
     getWord: function() {
 
+      var num = min(views);
+      views[num]++;
+      return Words[num];
+
     },
 
-    getLetters: function(wordLength, extraLetters) {
+    getLetters: function(word, extraLetters) {
+
+      return shuffle(word);
 
     }
 
--- src/words.service.tests.js
+++ src/words.service.tests.js
@@ -1,14 +1,16 @@
 describe('WordsService', function(){
 
+  var WS = WordsService;
+  
   it('should have a getLetters function', function(){
 
-    expect(WordsService.getLetters).toBeDefined();
+    expect(WS.getLetters).toBeDefined();
 
   });
 
   it('should have a getWord function', function(){
 
-    expect(WordsService.getWord).toBeDefined();
+    expect(WS.getWord).toBeDefined();
     
   });
 
@@ -16,9 +18,32 @@ describe('WordsService', function(){
     
     it('should return a word from the list of words', function(){
 
+      var word = WS.getWord();
+      expect(Words.indexOf(word)).not.toBe(-1);
+
+    });
+
+    it('should return a different word that the last time', function(){
+
+      for(var i=0; i < 100; i++) {
+        var word1 = WS.getWord();
+        var word2 = WS.getWord();
+        expect(word1).not.toEqual(word2);
+      }
+
     });
 
     it('should return the lesser know words for the user', function(){
+
+      Words = ['a', 'b', 'c'];
+      WS.init();
+
+      var word1 = WS.getWord();
+      var word2 = WS.getWord();
+      var word3 = WS.getWord();
+
+      expect(word1).not.toEqual(word2);
+      expect(word2).not.toEqual(word3);
 
     });
 
@@ -26,7 +51,14 @@ describe('WordsService', function(){
 
   describe('getLetters', function() { 
     
-    it('should return the letters from the word unsorted', function(){
+    it('should return the letters from the word, but unsorted', function(){
+
+      letters = WS.getLetters('word');
+      expect(letters.length).toEqual(4);
+      expect(letters.indexOf('w')).not.toEqual(-1);
+      expect(letters.indexOf('o')).not.toEqual(-1);
+      expect(letters.indexOf('r')).not.toEqual(-1);
+      expect(letters.indexOf('d')).not.toEqual(-1);
 
     });
 
